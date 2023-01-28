db3d5e253c8284b7a19e383298141a6927e90070
F1nefine <123521426+F1nefine@users.noreply.github.com>
2023-01-28 12:35:28 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+
+  $('.main__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+    prevArrow:'<button type="arrow" data-role="none" class="slick-prev"><img src="images/left.svg" alt=""></button>',
+    nextArrow:'<button type="arrow" data-role="none" class="slick-next"><img src="images/right.svg" alt=""></button>',
+
+  });
+})
